c9152ba20130a238df30487b926f576e8768df01
Amin <a.lotfi1201@gmail.com>
2026-05-14 15:01:37 +0330

    add form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
+import {FormComponent} from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    FormComponent
   ]
 })
 export class AppComponent {}
