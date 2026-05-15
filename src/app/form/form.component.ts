b24a93c60fb6eea7216f2d50d43c431d38cf2cf9
Amin <a.lotfi1201@gmail.com>
2026-05-15 17:30:48 +0330

    bind form data using ngModel

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {FormGroup, FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  initialInvestment = signal('')
+  annualInvestment = signal('')
+  expectedReturn = signal('5')
+  duration = signal('10')
 
+  onSubmit() {
+    console.log('form submitted', this.initialInvestment(), this.annualInvestment(), this.expectedReturn(), this.duration());
+  }
 }
